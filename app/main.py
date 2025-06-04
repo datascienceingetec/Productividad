6381a9631b23a9c7eb98d9f4f330c40726a561e7
Ariel Ernesto Forero Meneses <arielforero@ingetec.com.co>
2025-06-04 11:42:03 -0500

    add cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api.v1.endpoints import router as api_router
+from pydantic_settings import BaseSettings
+import os
 
-app = FastAPI(title="Productividad API")
+
+class Settings(BaseSettings):
+    app_name: str = "Productividad API"
+    frontend_url: str
+
+settings = Settings()
+app = FastAPI(title=settings.app_name)
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:5173", settings.frontend_url],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
